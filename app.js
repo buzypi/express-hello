b79192711e2f0cd1f034b3c0470767ee9b2084c6
Gautham Pai <buzypi@gmail.com>
2021-08-02 10:16:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello - today is May 20th - revision 2'))
+app.get('/', (req, res) => res.send('Hello - today is Aug 2nd!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
